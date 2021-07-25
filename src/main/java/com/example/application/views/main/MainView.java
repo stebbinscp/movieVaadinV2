e530b0bc8e2b08157c7eedbfaaf952e7b65cf9be
stebbinscp <cstebbins@uchicago.edu>
2021-07-25 19:19:41 -0400

    refactored cardlistview
    no cards being returned
    nothing else being retreived so nothing is being populated

---
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -0,0 +1,25 @@
+//package com.example.application.views.main;
+//
+//import com.vaadin.flow.component.applayout.AppLayout;
+//import com.vaadin.flow.component.dependency.CssImport;
+//import com.vaadin.flow.component.dependency.JsModule;
+//import com.vaadin.flow.component.html.H1;
+//import com.vaadin.flow.component.page.Push;
+//import com.vaadin.flow.component.tabs.Tabs;
+//import com.vaadin.flow.theme.Theme;
+//import com.vaadin.flow.theme.lumo.Lumo;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//@Push
+//@JsModule("./styles/shared-styles.js")
+//@Theme(value = Lumo.class, variant = Lumo.DARK)
+//@CssImport("./views/main.css")
+//
+//public class MainView extends AppLayout {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(AppLayout.class);
+//
+//
+//
+//}
---
+++ src/main/java/com/example/application/views/movies/MoviesView.java
@@ -0,0 +1,23 @@
+package com.example.application.views.movies;
+
+
+//import com.example.application.views.main.MainView;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.PreserveOnRefresh;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+
+//
+//@Route(value = "card-list", layout = MainView.class)
+//@RouteAlias(value = "", layout = MainView.class)
+//@PageTitle("Book Search")
+//@CssImport("./views/generic-list.css")
+//@PreserveOnRefresh
+//
+//public class MoviesView {
+//
+//
+//
+//}
